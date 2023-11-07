d42e9162127b81d70a0a922838b099b6cb0d4020
Rosny Fletez Jr. <rfletezjr@gmail.com>
2023-11-07 12:31:09 -0700

    Created frontend and backend folders

---
+++ backend/DinerPlanet/src/main/java/com/diner/DinerPlanet/DinerPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.diner.DinerPlanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinerPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinerPlanetApplication.class, args);
+	}
+
+}
---
+++ backend/DinerPlanet/src/test/java/com/diner/DinerPlanet/DinerPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diner.DinerPlanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinerPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
